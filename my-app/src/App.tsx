e31d32116582e1ce21cfb4462bfaee7444398f38
Yazan Al-Aswad <yazan.al-aswad@telavox.com>
2023-09-18 15:03:32 -0700

    styled login page

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,7 +3,6 @@ import { Login } from "./Login";
 export const App = (): JSX.Element => {
   return (
     <div>
-      This is the app
       <Login />
     </div>
   );
--- my-app/src/Login.tsx
+++ my-app/src/Login.tsx
@@ -1,21 +1,41 @@
 import { useState } from "react";
+import "./Login.css"
 
 export const Login = (): JSX.Element => {
   const [username, setUsername] = useState<string>("");
   const [password, setPassword] = useState<string>("");
 
   return (
+    
     <div className="login">
-      <input
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        type="text"
-      />
-      <input
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        type="passwor"
-      />
+      <div className="background">
+        <div className="shape"></div>
+        <div className="shape"></div>
+      </div>
+      <form>
+        <h3>Login Here</h3>
+
+        <label htmlFor="username">Username</label>
+        <input 
+          type="text"  
+          placeholder="Email or Phone"  
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <label htmlFor="password">Password</label>
+        <input 
+          type="password"  
+          placeholder="Password" 
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button>Log In</button>
+
+      </form>
     </div>
   );
 };
