c6fede1202614291bdf4d75e0fd1f36663853b69
Niklas Fähnrich <niklas.fahnrich@telavox.com>
2023-09-18 15:04:40 +0200

    wip

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,10 +1,27 @@
 import { Login } from "./Login";
 
 export const App = (): JSX.Element => {
+  const fetchLogos = async () => {
+    const url = "/api/internal/v2/branding/logo";
+    const response = await fetch(url);
+    const data = await response.json();
+
+    console.log(data);
+  };
+
+  const fetchCustomers = async () => {
+    const url = "/api/internal/customers";
+    const response = await fetch(url);
+    const data = await response.json();
+
+    console.log(data);
+  };
   return (
     <div>
       This is the app
       <Login />
+      <button onClick={fetchLogos}>Fetch Logos</button>
+      <button onClick={fetchCustomers}>Fetch hjasioudfghiuashdfgiashfgiuashfpiughsa</button>
     </div>
   );
 };
--- my-app/src/Login.tsx
+++ my-app/src/Login.tsx
@@ -4,18 +4,44 @@ export const Login = (): JSX.Element => {
   const [username, setUsername] = useState<string>("");
   const [password, setPassword] = useState<string>("");
 
+  const url = "/api/internal/admin/login";
+
+  const login = async () => {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Accept-Language": "",
+        "X-TVXClientType": "admin",
+        Authorization:
+          "Basic " + btoa(username + ":" + encodeURIComponent(password)),
+      },
+    });
+
+    const data = await response.json;
+    console.log(data);
+  };
+
+  const handleOnSubmit: React.FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    console.log("submitting");
+    login();
+  };
+
   return (
     <div className="login">
-      <input
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        type="text"
-      />
-      <input
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        type="passwor"
-      />
+      <form onSubmit={handleOnSubmit}>
+        <input
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          type="text"
+        />
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+        />
+        <button type="submit">login</button>
+      </form>
     </div>
   );
 };
